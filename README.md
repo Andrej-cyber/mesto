8dee93bf9dac04f2201ed9b01bb650bb92af0711
Andrej-cyber <andvit94@gmail.com>
2022-01-06 13:05:41 +0100

    feat:submit/func is done

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-# Проект: Место
+# Project: Mesto
 
-### Обзор
+[My site is published here](https://andrej-cyber.github.io/mesto/)
 
-* Figma
-* Картинки
+## Features
 
-**Figma**
+- This project has been created using HTML, CSS, js.
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+## Project Structure
 
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+- This project is based on Nested BEM(Block, Element, Modifier).
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const closeButton = document.querySelector('.popup__close-button');
 const popUp = document.querySelector('.popup');
 const popupOpenedClass = 'popup_opened';
 
-
-
 editButton.addEventListener('click', function(event) {
     event.preventDefault();
     popUp.classList.add(popupOpenedClass);
@@ -17,7 +15,6 @@ closeButton.addEventListener('click', function() {
     document.body.style.overflow = '';
 });
 
-
 document.addEventListener('keydown', function(event) {
     if (event.code === 'Escape') {
         popUp.classList.remove(popupOpenedClass);
@@ -27,29 +24,24 @@ document.addEventListener('keydown', function(event) {
 
 const submitButton = document.querySelector('.popup__submit-button');
 
+
 submitButton.addEventListener('click', function() {
     popUp.classList.remove(popupOpenedClass);
+    document.body.style.overflow = '';
+
 });
 
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('#input-name');
 let jobInput = document.querySelector('#input-job');
-
-
-
-
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
 
-
-
 function formSubmitHandler(evt) {
-
     evt.preventDefault();
 
-
-
-
+    profileName.textContent = nameInput.value;
+    profileStatus.textContent = jobInput.value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
