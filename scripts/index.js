06075cd61a85ef951986fb2eb1624d1a1b325176
Andrej-cyber <andvit94@gmail.com>
2022-01-19 17:36:36 +0100

    WIP:popup/add-pic

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,22 @@ let nameInput = document.querySelector('#input-name');
 let jobInput = document.querySelector('#input-job');
 let profileName = document.querySelector('.profile__name');
 let profileStatus = document.querySelector('.profile__status');
+// ------------------popupAdd-pic------------------------------
+const addButton = document.querySelector('.profile__add-button');
+const popUpAddPic = document.querySelector('.popup__add-pic');
+const closePopupAddButton = document.querySelector('.popup__close-button-add-pic');
+let pictureNameInput = document.querySelector('#input-pic-name');
+let pictureLinkInput = document.querySelector('#input-pic-link');
+let elementTitle = document.querySelectorAll('.element__title');
+let elementImage = document.querySelectorAll('.element__image');
+let formSubmitPictureElement = document.querySelector('.popup__add-pic-container');
+const submitPictureButton = document.querySelector('.popup__submit-pic-button');
+// -------------------------popup------------------------
+
+
+console.log({ formSubmitPictureElement });
+
+
 
 function openPopup() {
     popUp.classList.add(popupOpenedClass);
@@ -26,12 +42,37 @@ function formSubmitHandler(evt) {
     profileStatus.textContent = jobInput.value;
     closePopup();
 };
+// -------------------------popuAdd-pic----------------------
+function openPopupAddPicture() {
+    popUpAddPic.classList.add(popupOpenedClass);
+    pictureNameInput.value = pictureNameInput.textContent;
+    pictureLinkInput.value = pictureLinkInput.textContent;
+};
 
+
+function closePopupAddPicture() {
+    popUpAddPic.classList.remove(popupOpenedClass);
+};
+
+function formSubmitPictureHandler(evt) {
+    evt.preventDefault();
+    pictureNameInput.textContent = elementTitle.value;
+    pictureLinkInput.textContent = elementImage.value;
+    closePopupAddPicture();
+};
+// ----------------------EventListeners--------------------------
+addButton.addEventListener('click', openPopupAddPicture);
+closePopupAddButton.addEventListener('click', closePopupAddPicture);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+formSubmitPictureElement.addEventListener('submit', formSubmitPictureHandler);
 
 
+
+
+
+// ------------------------LIke Icon------------------------
 elementIcon.forEach((item) => {
     item.addEventListener('click', function (e) {
         e.target.classList.toggle('element__icon_active');
