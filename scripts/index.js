9899a229afb353a959b88f3ee198d473542b6161
Andrej-cyber <andvit94@gmail.com>
2022-02-05 18:14:06 +0100

    refactoring js & WIP:validate forms

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popupWindow = document.querySelector('.popup');
 const popupOpenedClass = 'popup_opened';
 const formElement = document.querySelector('.popup__container_type_profile');
-const nameInput = document.querySelector('#input-name');
-const jobInput = document.querySelector('#input-job');
+const nameInput = document.querySelector('.popup__text_type_name');
+const jobInput = document.querySelector('.popup__text_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const profilePopup = document.querySelector('.popup_type_profile');
@@ -11,8 +10,8 @@ const addButton = document.querySelector('.profile__add-button');
 const popUpAddImg = document.querySelector('.popup_type_card-add');
 const elementContainer = document.querySelector('.elements__container');
 const addElementForm = document.querySelector('.popup__container_type_add-img');
-const pictureNameInput = document.querySelector('#input-pic-name');
-const pictureLinkInput = document.querySelector('#input-pic-link');
+const pictureNameInput = document.querySelector('.popup__text_type_card-name');
+const pictureLinkInput = document.querySelector('.popup__text_type_card-link');
 const elementsTemplate = document.querySelector('#elements-template').content.querySelector('.element');
 
 function openPopup(popupWindow) {
@@ -29,11 +28,7 @@ function openProfilepopup() {
     openPopup(profilePopup);
 };
 
-function closeProfilePopup() {
-    closePopup(profilePopup);
-};
-
-function formSubmitHandler(evt) {
+function profileFormSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileStatus.textContent = jobInput.value;
@@ -49,17 +44,30 @@ function openPopupAddImg() {
 function closePopupAddImg() {
     closePopup(popUpAddImg);
 };
+// ---------------------------------------Close Popup I----------------------
+// const popupCloseButton = document.querySelectorAll('.popup__close-button');
+// if (popupCloseButton.length > 0) {
+//     for (let i = 0; i < popupCloseButton.length; i++) {
+//         const btn = popupCloseButton[i];
+//         btn.addEventListener('click', function (e) {
+//             e.preventDefault();
+//             closePopup(btn.closest('.popup'));
+//         });
+//     }
+// }
+// -----------------------------------------Close Popup II ----------------------------------
+// Благодаря такой реализации на следующем спринте будет проще реализовать дополнительный функционал, дописав еще одно условие.
 
-const popupCloseButton = document.querySelectorAll('.popup__close-button');
-if (popupCloseButton.length > 0) {
-    for (let i = 0; i < popupCloseButton.length; i++) {
-        const btn = popupCloseButton[i];
-        btn.addEventListener('click', function (e) {
-            e.preventDefault();
-            closePopup(btn.closest('.popup'));
-        });
-    }
-}
+const popupList = Array.from(document.querySelectorAll('.popup'));
+popupList.forEach((popup) => {
+    popup.addEventListener('click', (event) => {
+        if (event.target.classList.contains('popup__close-button')) {
+            closePopup(popup);
+        }
+    });
+});
+// ------------------------------------------------------------------------------------
+
 
 const getElement = ({ link, name }) => {
     const element = elementsTemplate.cloneNode(true);
@@ -122,5 +130,5 @@ initialCards.forEach(item => {
 addElementForm.addEventListener('submit', handleElementFormSubmit);
 editButton.addEventListener('click', openProfilepopup);
 addButton.addEventListener('click', openPopupAddImg);
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', profileFormSubmitHandler);
 
